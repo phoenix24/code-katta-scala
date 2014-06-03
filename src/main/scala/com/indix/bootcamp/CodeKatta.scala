3bf1d1fb8c93617e5e8247b3a7327228083838df
Chaitanya Sharma <gopi.daiict@gmail.com>
2014-06-03 13:37:53 +0530

    removing redundant import.

--- src/main/scala/com/indix/bootcamp/CodeKatta.scala
+++ src/main/scala/com/indix/bootcamp/CodeKatta.scala
@@ -1,9 +1,5 @@
 package com.indix.bootcamp
 
-import edu.uci.ics.crawler4j.crawler.{CrawlConfig, CrawlController}
-import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer
-import edu.uci.ics.crawler4j.fetcher.PageFetcher
-
 class CodeKatta {
 
 }
